4beba4f02769b8243920d30a40e4d2943f913421
Shobana <shobivijay90@gmail.com>
2019-04-22 11:10:58 -0700

    changes made on the heading

--- Js-program.js
+++ Js-program.js
@@ -1,4 +1,6 @@
-//Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3, "buzz" at multiples of 5 and "fizzbuzz" at multiples of 3 and 5
+//Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3, 
+//"buzz" at multiples of 5 and 
+//"fizzbuzz" at multiples of 3 and 5
 
 for (i=1; i<=100; i++){
   if(i%3==0 & i%5==0){
